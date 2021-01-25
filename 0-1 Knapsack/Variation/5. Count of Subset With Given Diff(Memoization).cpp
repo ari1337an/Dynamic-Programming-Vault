52be35c0f358e99b9a437cf64a57c00b08667a7b
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2021-01-25 17:57:30 +0600

    Create 5. Count of Subset With Given Diff(Memoization).cpp

---
+++ 0-1 Knapsack/Variation/5. Count of Subset With Given Diff(Memoization).cpp
@@ -0,0 +1,21 @@
+unordered_map<int,unordered_map<int,int>> dp;
+int solve(vector<int> & arr, long long int sum, int n){
+    if(sum == 0 && n == 0) return 1;
+    if(n == 0) return 0;
+    if(dp[sum][n]) return dp[sum][n];
+    if(arr[n-1] <= sum){
+        return dp[sum][n] = solve(arr,sum-arr[n-1],n-1) + solve(arr,sum,n-1);
+    }else{
+        return dp[sum][n] = solve(arr,sum,n-1);
+    }
+}
+int countSubsetSumGivenDiff(vector<int>& nums, int diff) {
+    dp.clear();
+    long long int sum = accumulate(nums.begin(),nums.end(),0);
+    sum = sum + diff;
+    if(sum%2 != 0){
+        return 0;
+    }
+    sum = sum / 2LL;
+    return solve(nums, sum, nums.size());
+}
