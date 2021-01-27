51b06d14eb75d32c46f4d2cd0b5b1d1670921f3c
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2021-01-27 19:31:52 +0600

    Update 1. Subset Sum(Memoization).cpp

--- 0-1 Knapsack/Variation/1. Subset Sum(Memoization).cpp
+++ 0-1 Knapsack/Variation/1. Subset Sum(Memoization).cpp
@@ -1,21 +1,3 @@
-/*
- * Parent Problem: 0-1 Knapsack
- * Problem Name: Subset Sum 
- * Type: Memoization DP
- * 
- * Problem Link: 
- */
- #include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
-#define ull unsigned long long
-#define all(x) x.begin(),x.end()
-#define MOD 1000000007
-#define PI acos(-1)
-#define debug cerr <<
-#define var(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define close << "\n";
-
 int dp[30+1][6+1];
 
 bool isSubsetSum(int arr[], int sum, int n){
@@ -37,16 +19,4 @@ bool isSubsetSum(int arr[], int sum, int n){
 			dp[sum][n] = 0;
 	}
 	return ((dp[sum][n] == 1) ? 1 : 0);
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-	//freopen("../input.txt","r",stdin);
-
-	memset(dp,-1,sizeof dp);
-	int arr[] = {3, 34, 4, 12, 5, 2};
-	int sum = 9;
-
-	cout << ((isSubsetSum(arr,sum,6))?"YES":"NO") close
-
 }
